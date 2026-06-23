06754e9a2d4875608c280ac2be0de40ec895153a
zyad <zyadbassem9090@gmail.com>
2026-06-23 15:50:05 +0300

    basic product

--- src/controllers/traineeControllers.ts
+++ src/controllers/traineeControllers.ts
@@ -81,8 +81,6 @@ export const generateQr = catchAsync(
       const secured = crypto.randomUUID();
       const data = {
         traineeId,
-        currentTime: Date.now(),
-        used: false,
         token: secured,
       };
       const stringfiedData = JSON.stringify(data);
@@ -100,5 +98,80 @@ export const generateQr = catchAsync(
     } else {
       throw new AppError("A qr code was generated before", 403);
     }
+  }
+);
+
+export const checkIn = catchAsync(
+  async (req: express.Request, res: express.Response) => {
+    const { payload } = req.body;
+    const HOLDING_HOURS = 2;
+    if (!payload || !payload.traineeId || !payload.token)
+      throw new AppError("Please scan again", 401);
+
+    const [trainee] = await db
+      .select()
+      .from(traineesTable)
+      .where(eq(traineesTable.id, payload.traineeId));
+    if (!trainee) throw new AppError("Trainee doesn't exist", 404);
+
+    if (payload.token !== trainee.lastQr)
+      throw new AppError("Invalid Token", 403);
+
+    const currTime = Date.now();
+    if (
+      trainee.lastCheckIn &&
+      currTime - trainee.lastCheckIn?.getTime() < HOLDING_HOURS * 60 * 60 * 1000
+    )
+      throw new AppError("Please check in again later", 403);
+
+    if (
+      !trainee.membershipExpiryDate ||
+      trainee.membershipStatus === "expired" ||
+      new Date() > trainee.membershipExpiryDate
+    ) {
+      await db
+        .update(traineesTable)
+        .set({ membershipStatus: "expired" })
+        .where(eq(traineesTable.id, trainee.id));
+      throw new AppError("Membership expired", 401);
+    }
+
+    // Update the db
+    await db
+      .update(traineesTable)
+      .set({ lastCheckIn: new Date(), lastQr: null })
+      .where(eq(traineesTable.id, trainee.id));
+
+    res
+      .status(200)
+      .json({ message: "Welcome to the gym", trainee: trainee.name });
+  }
+);
+
+export const renewMembership = catchAsync(
+  async (req: express.Request, res: express.Response) => {
+    const { traineeId, numOfDays } = req.body;
+    if (!traineeId || !numOfDays)
+      throw new AppError("Please enter the missing fields", 400);
+
+    const [trainee] = await db
+      .select()
+      .from(traineesTable)
+      .where(eq(traineesTable.traineeId, traineeId))
+      .limit(1);
+
+    if (!trainee) throw new AppError("Please enter a valid trainee id", 400);
+    const currDate = new Date();
+    const updatedDate =
+      trainee.membershipExpiryDate && currDate < trainee.membershipExpiryDate
+        ? trainee.membershipExpiryDate
+        : currDate;
+    updatedDate.setDate(updatedDate.getDate() + numOfDays);
+    await db
+      .update(traineesTable)
+      .set({ membershipExpiryDate: updatedDate, membershipStatus: "valid" })
+      .where(eq(traineesTable.traineeId, traineeId));
+
+    res.status(200).json({ message: "Renewed Membership", updatedDate });
   }
 );
--- src/routes/traineeRoutes.ts
+++ src/routes/traineeRoutes.ts
@@ -1,10 +1,22 @@
 import { Router } from "express";
 import { requireAdmin, requireTrainee } from "../middlewares/authMiddleware.js";
 import {
+  checkIn,
   createTrainee,
   generateQr,
+  renewMembership,
 } from "../controllers/traineeControllers.js";
 export const traineeRouter = Router();
 
 traineeRouter.post("/", requireAdmin, createTrainee);
 traineeRouter.get("/qr/new", requireTrainee, generateQr);
+traineeRouter.post("/checkin", requireAdmin, checkIn);
+traineeRouter.put("/membership", requireAdmin, renewMembership);
+
+// {
+// 	"payload":
+//  {
+// 		"token": "779c2553-fe53-435b-aa0e-99403e290f85",
+// 		"traineeId": "34d6c8a6-b768-440d-966f-069dae6c7511"
+// 	}
+// }
